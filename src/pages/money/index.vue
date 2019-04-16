7ace3e9554eaf14bb0f54c47757355383bdbfb36
xxx0527 <3466392059@qq.com>
2019-04-16 23:16:56 +0800

    init

---
+++ src/pages/money/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div @click="clickHandle">
+      <i-grid i-class="no-border">
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/钱包.png" />
+        </i-grid-icon>
+        <i-grid-label>钱包</i-grid-label>
+    </i-grid-item>
+     <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/信用卡.png" />
+        </i-grid-icon>
+        <i-grid-label>信用卡</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/余额宝.png" />
+        </i-grid-icon>
+        <i-grid-label>余额宝</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+            <image src="/static/grid/优惠券.png" />
+        </i-grid-icon>
+        <i-grid-label>优惠券</i-grid-label>
+    </i-grid-item>
+      </i-grid>
+</div>
+ </template>

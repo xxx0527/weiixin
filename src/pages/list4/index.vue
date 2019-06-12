aef853637f25c1eeacb20a68b47637b792e40c2b
xxx0527 <3466392059@qq.com>
2019-06-12 17:04:04 +0800

    init

--- src/pages/list4/index.vue
+++ src/pages/list4/index.vue
@@ -1,149 +1,94 @@
 <template>
   <div @click="clickHandle">
 
-    <i-notice-bar icon="systemprompt" loop>
-    欢迎进入小熊旅游网站，{{notice}}，这个夏天带你遨游世界！！！
-    </i-notice-bar>
-    <view>
-      <swiper class="swiper" autoplay="true" interval="2000">
-        <swiper-item>
-          <image src="/static/images/上海.jpg"></image>
-        </swiper-item>
-        <swiper-item>
-          <image src="/static/images/乌镇.jpg"></image>
-          </swiper-item>
-          <swiper-item>
-            <image src="/static/images/普罗旺斯.jpg"></image>
-          </swiper-item>
-           <swiper-item>
-            <image src="/static/images/西岛.jpg"></image>
-          </swiper-item>
-          <swiper-item>
-            <image src="/static/images/西藏.jpg"></image>
-          </swiper-item>
-          <swiper-item>
-            <image src="/static/images/重庆.jpg"></image>
-          </swiper-item>
-         <swiper-item>
-            <image src="/static/images/凤凰古城.jpg"></image>
-          </swiper-item>
-     </swiper>
-   </view>
-
-    <i-grid i-class="no-border">
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="/static/grid/1.png" />
-        </i-grid-icon>
-        <i-grid-label>美食林</i-grid-label>
-    </i-grid-item>
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="/static/grid/2.png" />
-        </i-grid-icon>
-        <i-grid-label>旅游攻略</i-grid-label>
-    </i-grid-item>
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="/static/grid/3.png" />
-        </i-grid-icon>
-        <i-grid-label>交通出行</i-grid-label>
-    </i-grid-item>
-    </i-grid>
-    <i-grid i-class="no-border">
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="/static/grid/4.png" />
-        </i-grid-icon>
-        <i-grid-label>景点门票</i-grid-label>
-    </i-grid-item>
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="/static/grid/5.png" />
-        </i-grid-icon>
-        <i-grid-label>民宿酒店</i-grid-label>
-    </i-grid-item>
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-            <image src="/static/grid/6.png" />
-        </i-grid-icon>
-        <i-grid-label>更多</i-grid-label>
-    </i-grid-item>
-</i-grid>
-  <i-panel title="猜你喜欢">
-   
-<view class="card">
-  <text class="card-title">三亚</text>
-	<image class='card-img' src='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559564662248&di=03a27b19f1575472dcf2e6146cd0db09&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fupload%2Fupc%2Ftx%2Fphotoblog%2F1303%2F15%2Fc11%2F18947634_18947634_1363361408421.jpg'></image>
-	<text class="card-desc">情侣出游地</text>
-  <i-cell title="查看详情" is-link url="/pages/kefu/main"> </i-cell>
-</view>
- 
-<view class="card">
-	<text class="card-title">上海</text>
-	<image class='card-img' src='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559564957729&di=06fb1c9e25b43bbaabc869296ed50912&imgtype=0&src=http%3A%2F%2Fs1.sinaimg.cn%2Fmw690%2F002ySfIBzy754HXJQ1Wb0%26690'></image>
-	<text class="card-desc">家人一同出行</text>
-	<i-cell title="查看详情" is-link url="/pages/kefu/main"> </i-cell>
- </view>
-<view class="card">
-	<text class="card-title">长沙</text>
-	<image class='card-img' src='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559565277762&di=f182350620da56eb25aae2bb2111cbcc&imgtype=0&src=http%3A%2F%2Fwww.zhongt.com.cn%2Fupload%2FPd15650fc963d4946ac0b9579434a4e05.jpg'></image>
-	<text class="card-desc">吃货必打卡地</text>
-	<i-cell title="查看详情" is-link url="/pages/kefu/main"> </i-cell>
- </view>
-<view class="card">
-	<text class="card-title">香港</text>
-	<image class='card-img' src='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559565343760&di=2116661c0d155c5d971e3a45fc640910&imgtype=0&src=http%3A%2F%2Fm.tuniucdn.com%2Ffilebroker%2Fcdn%2Fsnc%2Fbb%2F55%2Fbb55637c1da6aa50c9dccbd183f056d1_w800_h0_c0_t0.jpg'></image>
-	<text class="card-desc">购物天堂</text>
-	<i-cell title="查看详情" is-link url="/pages/kefu/main"> </i-cell>
- </view>
-<view class="card">
-	<text class="card-title">西安</text>
-	<image class='card-img' src='https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559565383841&di=2f6278f1290b8a0a90833552f853c8a0&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fupload%2Fupc%2Ftx%2Fitbbs%2F1309%2F23%2Fc49%2F26198385_1379931537586.jpg'></image>
-	<text class="card-desc">体验古城别一般风韵</text>
- <i-cell title="查看详情" is-link url="/pages/kefu/main"> </i-cell>
-</view>
-
-<!-- <view class='shop'>
-    <image class='shop-img' src='http://p1.meituan.net/200.0/dpdeal/5f37afe9ef2dc9c68db49dbe9bfb6c15419781.jpg'></image>
+<view class='shop'>
+    <image class='shop-img' src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559715001191&di=9ece07f30c827f48d57824c12aea4e49&imgtype=0&src=http%3A%2F%2Fyouimg1.c-ctrip.com%2Ftarget%2F10080s000000hlyj9745C.jpg"></image>
     <view class='shop-info'>
-        <text class='shop-title ell'>三亚</text>
-        <text class='shop-tel'>地址：海南省南部</text>
-        <text class='shop-address'>情侣出游地</text>
-        <text class='shop-time'>建议游玩:六月份</text>
+        <text class='shop-title ell'>海洋水族馆</text>
+        <text class='shop-address'>地址:上海市浦东新区陆家嘴环路1388号</text>
+       <text class='shop-time'>开放时间：9:00-21:00</text>
+        <text class='shop-time'>建议游玩时长:1-3小时</text>
       
     </view>
     <view class='shop-star'>
         <view class='star-after'>★★★★★</view>
-        <view class='star-before' style="width:50%;">★★★★★</view>
+        <view class='star-before' style="width:90%;">★★★★★</view>
     </view>
  
 </view>
  
 <view class="shop">
-    <image class="shop-img" src="http://p0.meituan.net/200.0/deal/5511b4e5b25fdd67aea0bbca8656ec581139488.jpg@200_0_800_800a%7C267h_267w_2e_90Q"></image>
+    <image class="shop-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559715545156&di=192d14e8119e1211f074955e0cb10f97&imgtype=0&src=http%3A%2F%2Fp1.ssl.cdn.btime.com%2Ft0173110cc3f761f76a.jpg"></image>
     <view class="shop-info">
-        <text class="shop-title ell">上海</text>
-        <text class="shop-tel">地址：上海市</text>
-        <text class="shop-address">适合家人旅游</text>
-        <text class="shop-time">建议游玩：三，四月份</text>
+        <text class="shop-title ell">杜莎夫人蜡像</text>
+        <text class="shop-address">地址：黄浦区人民广场新世界商厦10楼</text>
+        <text class="shop-time">开放时间：10:00-21:00</text>
+        <text class='shop-time'>建议游玩时长:2-3小时</text>
     </view>
     <view class="shop-star">
         <view class='star-after'>★★★★★</view>
-        <view class='star-before' style="width:90%;">★★★★★</view>
+        <view class='star-before' style="width:85%;">★★★★★</view>
     </view>
-</view> -->
- 
- <!--  <view >
-       <i-card @click="goType(item.type)" i-class="split" v-for="item in recommand" :key="item" :extra="item.name" :thumb="item.img">
-          <view slot="content">建议游玩：{{item.time}}</view>
-          <view slot="footer">地址：{{item.address}}</view>
-      </i-card>
-    </view>  -->
-    
-  </i-panel>
+</view>
 
-  </div>
+<view class="shop">
+    <image class="shop-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559715801614&di=1911da81738ce2a76d4fc7ad9ca1690b&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fupload%2Fupc%2Ftx%2Fitbbs%2F1504%2F23%2Fc69%2F5847487_1429790469325_mthumb.jpg"></image>
+    <view class="shop-info">
+        <text class="shop-title ell">东方明珠</text>
+        <text class="shop-address">地址：浦东新区陆家嘴世纪大道1号</text>
+        <text class="shop-time">开放时间：8:00-22:00</text>
+        <text class='shop-time'>建议游玩时长:2-3小时</text>
+    </view>
+    <view class="shop-star">
+        <view class='star-after'>★★★★★</view>
+        <view class='star-before' style="width:92%;">★★★★★</view>
+    </view>
+</view>
+
+<view class="shop">
+    <image class="shop-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559714077174&di=4558e5161c2163a39e878c217877a4a9&imgtype=0&src=http%3A%2F%2Fimg.pconline.com.cn%2Fimages%2Fupload%2Fupc%2Ftx%2Fitbbs%2F1506%2F02%2Fc1%2F7809865_1433177624198.jpg"></image>
+    <view class="shop-info">
+        <text class="shop-title ell">亚龙湾</text>
+        <text class="shop-address">地址：亚龙湾国家旅游度假区</text>
+        <text class="shop-time">开放时间：8:00-17:30</text>
+        <text class='shop-time'>建议游玩时长:3-4天</text>
+    </view>
+    <view class="shop-star">
+        <view class='star-after'>★★★★★</view>
+        <view class='star-before' style="width:92%;">★★★★★</view>
+    </view>
+</view>
+
+<view class="shop">
+    <image class="shop-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559714234502&di=a43c710bccb9882c9ec28e1b6f2b8d0a&imgtype=0&src=http%3A%2F%2Fs8.sinaimg.cn%2Fmw690%2F003pK4uxty6Fv837p9te7%26690"></image>
+    <view class="shop-info">
+        <text class="shop-title ell">鸟巢度假村</text>
+        <text class="shop-address">地址：吉阳区亚龙湾国际旅游度假区</text>
+        <text class="shop-time">开放时间：全天</text>
+        <text class='shop-time'>建议游玩时长:1天</text>
+    </view>
+    <view class="shop-star">
+        <view class='star-after'>★★★★★</view>
+        <view class='star-before' style="width:88%;">★★★★★</view>
+    </view>
+</view>
+
+<view class="shop">
+    <image class="shop-img" src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1559714514584&di=603af805a7ff7c4a5beaf1c48c543178&imgtype=0&src=http%3A%2F%2Fdimg06.c-ctrip.com%2Fimages%2F100h0a0000004fkhi406D_R_1024_10000_Q90.jpg"></image>
+    <view class="shop-info">
+        <text class="shop-title ell">解放路步行街</text>
+        <text class="shop-address">地址：吉阳区</text>
+        <text class="shop-time">开放时间：全天</text>
+        <text class='shop-time'>建议游玩时长:1-2小时</text>
+    </view>
+    <view class="shop-star">
+        <view class='star-after'>★★★★★</view>
+        <view class='star-before' style="width:70%;">★★★★★</view>
+    </view>
+</view>
+
+
+    </div>
 </template>
 
 <script>
@@ -152,19 +97,7 @@ import top from '@/data/top.json'
 
 export default {
   data () {
-  
-    return {
-      notice:"夏日打卡地",
-      grids:[
-      {type:'三亚',img:'/static/images/三亚.jpg',"url":'../list/main?type=1'},
-      {type:'上海',img:'/static/images/上海.jpg',"url":'../list/main?type=2'},
-      {type:'长沙',img:'/static/images/长沙.jpg',"url":'../list/main?type=3'},
-      {type:'香港',img:'/static/images/香港.jpg',"url":'../list/main?type=4'},
-      {type:'西安',img:'/static/images/西安.jpg',"url":'../list/main?type=5'}
-      ],
-      recommand:top
-      }
-  
+
    },
   
 
@@ -173,13 +106,7 @@ export default {
   },
 
   methods: {
-   goList(url){
-       mpvue.navigateTo({ url })
-   },
-     goType(type){
-       let url='../list/main?type='+ type
-       mpvue.navigateTo({ url })  
-      }
+
     },
   
 
@@ -308,4 +235,58 @@ div >>> .no-border {
     border-right: 3rpx solid #ccc;
     transform: rotate(45deg);
 }
+
+.shop{
+  display: flex;
+  padding: 20rpx; 
+  flex-direction: row;
+  border-bottom:1rpx solid #ddd; 
+  position: relative;
+ 
+}
+.shop-info{
+  width:400rpx;
+  display: flex;
+  flex-direction: column;
+  font-size: 12px;
+  color: #666;
+  
+ 
+}
+.shop-img{
+  width: 160rpx;
+  height: 160rpx;
+  margin-right: 20rpx;
+}
+.shop-title{
+  font-size: 16px;
+  color: #333;
+   width: 380rpx;
+}
+ 
+ 
+/*单行超出宽度变省略号 */
+.ell{
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+}
+ 
+/* 星星评分 */
+.shop-star{
+  right: 20rpx;
+  top: 20rpx;
+  position: absolute;
+}
+.star-after{
+  color: #e2e2e2;
+}
+.star-before{
+  color: #f19e38;
+  position: absolute;
+  left: 0rpx;
+  top: 0rpx; 
+  overflow: hidden;
+}
+
 </style>
